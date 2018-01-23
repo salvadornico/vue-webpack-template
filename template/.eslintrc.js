8b894aa296a07e4f910191d03342808273096aad
Nico <salvador.nico@gmail.com>
2018-01-23 14:26:36 +0000

    Nico's changes

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,74 +1,110 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-  },
-  {{#if_eq lintConfig "standard"}}
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
-  {{/if_eq}}
-  {{#if_eq lintConfig "airbnb"}}
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential', 'airbnb-base'],
-  {{/if_eq}}
-  {{#if_eq lintConfig "none"}}
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential'],
-  {{/if_eq}}
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  {{#if_eq lintConfig "airbnb"}}
-  // check if imports actually resolve
-  settings: {
-    'import/resolver': {
-      webpack: {
-        config: 'build/webpack.base.conf.js'
-      }
-    }
-  },
-  {{/if_eq}}
-  // add your custom rules here
-  rules: {
-    {{#if_eq lintConfig "standard"}}
-    // allow async-await
-    'generator-star-spacing': 'off',
-    {{/if_eq}}
-    {{#if_eq lintConfig "airbnb"}}
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      js: 'never',
-      vue: 'never'
-    }],
-    // disallow reassignment of function parameters
-    // disallow parameter object manipulation except for specific exclusions
-    'no-param-reassign': ['error', {
-      props: true,
-      ignorePropertyModificationsFor: [
-        'state', // for vuex state
-        'acc', // for reduce accumulators
-        'e' // for e.returnvalue
-      ]
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      optionalDependencies: ['test/unit/index.js']
-    }],
-    {{/if_eq}}
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+		root: true,
+		parser: "vue-eslint-parser",
+		parserOptions: {
+			"parser": "babel-eslint",
+			"ecmaVersion": 2017,
+			sourceType: "module"
+		},
+		env: {
+			browser: true,
+		},
+		{
+			{#
+				if_eq lintConfig "standard"
+			}
+		}
+		extends: [
+			// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+			// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+			"plugin:vue/recommended",
+			// https://github.com/standard/standard/blob/master/docs/RULES-en.md
+			"eslint:recommended",
+		],
+		{
+			{
+				/if_eq}} {
+					{#
+						if_eq lintConfig "airbnb"
+					}
+				}
+				// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+				// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+				extends: ['plugin:vue/recommended', 'airbnb-base'], {
+						{
+							/if_eq}} {
+								{#
+									if_eq lintConfig "none"
+								}
+							}
+							// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+							// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+							extends: ['plugin:vue/recommended'], {
+									{
+										/if_eq}}
+										// required to lint *.vue files
+										plugins: [
+											'vue', 'html'
+										], {
+											{#
+												if_eq lintConfig "airbnb"
+											}
+										}
+										// check if imports actually resolve
+										settings: {
+												'import/resolver': {
+													webpack: {
+														config: 'build/webpack.base.conf.js'
+													}
+												}
+											}, {
+												{
+													/if_eq}}
+													// add your custom rules here
+													rules: {
+															{
+																{#
+																	if_eq lintConfig "standard"
+																}
+															}
+															// allow async-await
+															'generator-star-spacing': 'off',
+															{
+																{
+																	/if_eq}} {
+																		{#
+																			if_eq lintConfig "airbnb"
+																		}
+																	}
+																	// don't require .vue extension when importing
+																	'import/extensions': ['error', 'always', {
+																		js: 'never',
+																		vue: 'never'
+																	}],
+																	// disallow reassignment of function parameters
+																	// disallow parameter object manipulation except for specific exclusions
+																	'no-param-reassign': ['error', {
+																		props: true,
+																		ignorePropertyModificationsFor: [
+																			'state', // for vuex state
+																			'acc', // for reduce accumulators
+																			'e' // for e.returnvalue
+																		]
+																	}],
+																	// allow optionalDependencies
+																	'import/no-extraneous-dependencies': ['error', {
+																		optionalDependencies: ['test/unit/index.js']
+																	}], {
+																		{
+																			/if_eq}}
+																			// allow debugger during development
+																			'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+																				"arrow-parens": 0,
+																				"indent": ["error", "tab"],
+																				"quotes": ["error", "double"],
+																				"linebreak-style": ["warn", "unix"],
+																				"no-undef": "off"
+																		}
+																	}
