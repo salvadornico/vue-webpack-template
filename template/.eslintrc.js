5b6a4a2ad0b8c574556343bde94b01406eb47a9f
Nico <salvador.nico@gmail.com>
2018-01-23 14:44:41 +0000

    eslintrc fix

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -28,8 +28,7 @@ module.exports = {
 	{{# if_eq lintConfig "none" }}
 	// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
 	// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-	extends: ['plugin:vue/recommended'],
-	{{ /if_eq }}
+	extends: ['plugin:vue/recommended'],{{ /if_eq }}
 	// required to lint *.vue files
 	plugins: [
 		'vue', 'html'
